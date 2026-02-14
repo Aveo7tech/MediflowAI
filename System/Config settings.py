0fef3c336e379e80344cfe70883f5407485689c8
Aveo7tech <kryptonat1905@gmail.com>
2026-02-14 12:06:04 +0530

    Add environment variable

--- System/Config settings.py
+++ System/Config settings.py
@@ -1,1 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    COMPOSIO_API_KEY = os.getenv("COMPOSIO_API_KEY")
+    MODEL_NAME = "gpt-4o"
+    TEMPERATURE = 0
+
+settings = Settings()
 
